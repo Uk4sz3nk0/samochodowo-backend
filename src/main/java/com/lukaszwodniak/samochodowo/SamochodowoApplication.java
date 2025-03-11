c4177e5eec08f39ff42fef96bc26a90b45792a87
Łukasz Wodniak <gerdzio53@gmail.com>
2025-03-11 23:31:46 +0100

    Initial commit

---
+++ src/main/java/com/lukaszwodniak/samochodowo/SamochodowoApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszwodniak.samochodowo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamochodowoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamochodowoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukaszwodniak/samochodowo/SamochodowoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszwodniak.samochodowo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamochodowoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
