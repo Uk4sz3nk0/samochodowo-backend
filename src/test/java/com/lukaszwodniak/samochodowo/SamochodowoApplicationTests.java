376a4d3e5997d43517e1a8b496c5eb809c7adaa8
Łukasz Wodniak <gerdzio53@gmail.com>
2025-06-20 14:59:14 +0200

    Fixed test workflow with Java distribution

--- src/test/java/com/lukaszwodniak/samochodowo/SamochodowoApplicationTests.java
+++ src/test/java/com/lukaszwodniak/samochodowo/SamochodowoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.lukaszwodniak.samochodowo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class }) // Only if not using database in tests
 class SamochodowoApplicationTests {
 
 	@Test
