8f80d1ece8d791fe86a0f1ac94964730e208ad9c
Łukasz Wodniak <gerdzio53@gmail.com>
2025-07-10 20:39:42 +0200

    [FEATURE] - Implementing manufacturers and models

--- src/test/java/com/lukaszwodniak/samochodowo/unit/BaseUnitTest.java
+++ src/test/java/com/lukaszwodniak/samochodowo/unit/BaseUnitTest.java
@@ -1,24 +1,10 @@
 package com.lukaszwodniak.samochodowo.unit;
 
-import com.lukaszwodniak.samochodowo.models.entity.Manufacturer;
+import com.lukaszwodniak.samochodowo.TestCommons;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.UUID;
-
 @ExtendWith(MockitoExtension.class)
-public abstract class BaseUnitTest {
+public abstract class BaseUnitTest extends TestCommons {
 
-    protected static final String TOYOTA_MANUFACTURER = "Toyota";
-
-    protected Manufacturer generateManufacturer(UUID id, String name) {
-        Manufacturer manufacturer = new Manufacturer();
-        manufacturer.setName(name);
-        manufacturer.setId(id);
-        return manufacturer;
-    }
-
-    protected Manufacturer getToyotaManufacturer(UUID id) {
-        return generateManufacturer(id, TOYOTA_MANUFACTURER);
-    }
 }
